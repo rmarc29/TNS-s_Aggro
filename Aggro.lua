ba06dad7af51cbc6871b9848974eca142ff10dad
rmarc29 <164156946+rmarc29@users.noreply.github.com>
2025-06-12 14:33:33 +0200

    Targeting fix

--- Aggro.lua
+++ Aggro.lua
@@ -1,31 +1,38 @@
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("UNIT_TARGET")
+frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 
 -- Warning text
-
 local warningText = frame:CreateFontString(nil, "OVERLAY")
 warningText:SetFont("Fonts/FRIZQT__.TTF", 32, "OUTLINE")
 warningText:SetPoint("CENTER", UIParent, "CENTER", 0, 100)
 warningText:SetTextColor(1, 0, 0)
-warningText:SetText("You're being targeted by " .. unit)
 warningText:Hide()
 
--- Time text
+-- Warning icon
+local warningIcon = frame:CreateTexture(nil, "OVERLAY")
+warningIcon:SetSize(64, 64)
+warningIcon:SetPoint("CENTER", UIParent, "CENTER", 0, 200)
+warningIcon:SetTexture("Interface\\Icons\\Ability_DualWield")
+warningIcon:Hide()
 
-local function ShowWarning()
+-- Show warning (text + icon + sound)
+local function ShowWarning(unit)
+    local name = UnitName(unit) or "an enemy"
+    warningText:SetText("You're being targeted by " .. name)
     warningText:Show()
-    PlaySoundFile("Sound//Item//Weapons//Gun//GunFire01.wav")
+    warningIcon:Show()
+    PlaySoundFile("Sound\\Item\\Weapons\\Gun\\GunFire01.wav")
+
     C_Timer.After(2, function()
         warningText:Hide()
+        warningIcon:Hide()
     end)
 end
 
--- Unit target check
-
-frame:SetScript("OnEvent", 
-    function(self, event, unit)
-        if UnitCanAttack("player", unit) and UnitIsUnit(unit.."target", "player")
-    then
-        ShowWarning()
+-- Event handler
+frame:SetScript("OnEvent", function(self, event, unit)
+    if UnitCanAttack("player", "target") and UnitIsUnit("targettarget", "player") then
+        ShowWarning("target")
     end
 end)
